f19bf5cf396174d94d0f22f5fa282881f2f0dfcb
Susan M. McMillan <smcmillan2@wisc.edu>
2022-07-26 13:12:49 -0500

    Corrected typo

---
+++ Edit dot Rprofile.R
@@ -0,0 +1,9 @@
+
+#  .Renviron
+usethis::edit_r_profile()
+
+#  .Rprofile (for projects)
+usethis::edit_r_profile("project")
+
+#  Point to r-universe fo rpackage installation
+usethis::edit_r_profile(scop = "project")
